c70860dda446d3a9e18fb8e8c5d9f80d6a5de8af
Dmitry <d.bychkov.95@mail.ru>
2018-12-16 21:03:55 +0300

    My first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SayFullName name="Dmitry" surname="Kek" link="vk.com" />
+        <SayFullName name="Lol" surname="qwer" link="#" />
       </div>
-    );
+      )
   }
+}
+
+function SayFullName(props) {
+  return (
+    <div>
+      <h1>Name - {props.name}, Surname - {props.surname}</h1>
+      <a href={props.link}>My VK</a>
+    </div>
+    )
 }
 
 export default App;
