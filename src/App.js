4602a82765d3ee296432aec44279e6fecac37944
Dmitry <d.bychkov.95@mail.ru>
2018-12-25 22:45:54 +0300

    lastChange

--- src/App.js
+++ src/App.js
@@ -3,28 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+
 import styled from 'styled-components';
+
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+const MainWrapper = styled.main`
+    height: 600px;
+    padding-top 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <SayFullName name="Dmitry" surname="Kek" link="vk.com" />
-        <SayFullName name="Lol" surname="qwer" link="#" />
-      </div>
-      )
+        <div className='App'>
+          <HeaderWrapper>
+            <Grid>
+              <Header />
+            </Grid>
+          </HeaderWrapper>
+          <MenuWrapper>
+            <Grid>
+              <Menu />
+            </Grid>
+          </MenuWrapper>
+          <MainWrapper>
+            <Grid>
+              <Main />
+            </Grid>
+          </MainWrapper>
+        </div>
+      );
   }
-}
-
-function SayFullName(props) {
-  return (
-    <div>
-      <h1>Name - {props.name}, Surname - {props.surname}</h1>
-      <a href={props.link}>My VK</a>
-    </div>
-    )
 }
 
 export default App;
